41da36fbd51ed895d80e9c3a1af448095b39f0f9
bingbing <2432835715@qq.com>
2020-10-14 22:52:02 +0800

    1.options全局参数定义学习

---
+++ options/demo01.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+
+import tornado.web
+
+# 使用options全局定义参数
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world")
+        print("调用成功!")
+
+
+def make_app():
+    return tornado.web.Application(
+        [
+            (r"/hello", MainHandler),
+        ]
+    )
+
+
+def main():
+    app = make_app()
+    app.listen(8008)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
