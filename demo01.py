d2fcb79e160fa513735ef2a740ace990ff91b054
bingbing <2432835715@qq.com>
2020-10-14 22:16:02 +0800

    1.第一个hello_word程序

---
+++ demo01.py
@@ -0,0 +1,27 @@
+import tornado.ioloop
+
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world")
+        print("调用成功!")
+
+
+def make_app():
+    return tornado.web.Application(
+        [
+            (r"/hello", MainHandler),
+        ]
+    )
+
+
+def main():
+    app = make_app()
+    app.listen(8008)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
