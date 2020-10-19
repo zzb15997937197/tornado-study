a6d0288acb8e816f9b7f63182bb9432a518b7dcc
zhangzhengbing <zhangzhengbing@huayin.com>
2020-10-19 10:27:20 +0800

    1. websocket使用

--- day01/security/login_demo.py
+++ day01/security/login_demo.py
@@ -1,5 +1,8 @@
 '''
 使用get_secure_cookie()和set_secure_cookie()替换原来的get_cookie()和set_cookie()方法
+案例实现:  身份验证框架
+1) 用户大于等于3个即可登录。
+
 '''
 import tornado.web
 from tornado.web import Application
@@ -11,13 +14,16 @@ dict_sessions = {}
 
 class BaseHandler(tornado.web.RequestHandler):
 
-    # current_user为 RequestHandler里方法
+    # current_user为 RequestHandler里的属性，为只读，因此需要使用get_current_user()方法为其赋值。
     def get_current_user(self):
-        print([x for x in dict_sessions])
-        session_id = self.get_secure_cookie("session_id")
+        print(dict_sessions)
+        session_id = self.get_secure_cookie("session")
         if session_id is None:
-            return "无用户"
-        return dict_sessions[session_id]
+            return
+        session = session_id.decode("utf-8")
+        if session not in dict_sessions:
+            return
+        return dict_sessions[session]
 
 
 class MainHandler(BaseHandler):
@@ -25,7 +31,7 @@ class MainHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         name = tornado.escape.xhtml_escape(self.current_user)
-        self.write("hello" + name)
+        self.write("hello,用户:" + name + "!")
 
 
 class LoginHandler(BaseHandler):
@@ -48,17 +54,17 @@ class LoginHandler(BaseHandler):
 
     # 验证用户是否允许登录，不允许登录那么跳转至登录页面
     def post(self, *args, **kwargs):
-        # 登录规则，名字长度为3个才允许登录
+        # 登录规则，名字长度大于或等于3个才允许访问，否则需要输入用户名和密码
         name = self.get_argument("name")
         if len(name) < 3:
             self.redirect("/login")
             session_id = str(uuid.uuid4())
-            dict_sessions[session_id] = name
-            self.set_cookie("session_id", session_id)
-            print(self.get_cookie("session_id"))
-            print("登录成功!")
+            self.set_secure_cookie("session", session_id)
+            dict_sessions[self.get_secure_cookie("session").decode("utf-8")] = name
+            print(self.get_cookie("session"))
+            print(self.get_secure_cookie("session"))
         else:
-            print("name无效,请输出长度大于3的name!")
+            self.write("允许访问!")
 
 
 if __name__ == "__main__":
---
+++ day01/websocket/server.py
@@ -0,0 +1,70 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+from tornado import gen
+
+from tornado.options import define, options, parse_command_line
+
+define("port", default=8888, help="run on given port", type=int)
+
+# 定义一个空的字典
+clients = dict()
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    # 异步访问
+    @tornado.web.asynchronous
+    def get(self):
+        print("访问系统首页...")
+        self.render("index.html")
+
+
+class MyWebSocketHandler(tornado.websocket.WebSocketHandler):
+
+    # 有新的连接时open()函数将会被调用,将客户端的连接统一放到clients
+    def open(self, *args, **kwargs):
+        self.id = self.get_argument("id")
+        self.stream.set_nodelay(True)
+        clients[self.id] = {"id": self.id, "object": self}
+        print(clients)
+        print("建立连接...")
+
+    # 客户收到消息将被调用
+    def on_message(self, message):
+        print("client %s received a message: %s" % (self.id, message))
+
+    # 关闭连接时被调用
+    def on_close(self):
+        if self.id in clients:
+            del clients[self.id]
+        print("client %s is closed" % self.id)
+
+    def check_origin(self, origin):
+        return True
+
+
+import threading
+import time
+import datetime
+
+
+def send_time():
+    while True:
+        for key in clients.keys():
+            msg = str(datetime.datetime.now())
+            clients[key]["object"].write_message(str)
+            print("write to client %s:%s" % (key, msg))
+        time.sleep(1)
+
+
+app = tornado.web.Application([
+    (r"/", IndexHandler),
+    (r"/websocket", MyWebSocketHandler)
+])
+
+if __name__ == "__main__":
+    threading.Thread(target=send_time()).start()
+    # parse_command_line()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
